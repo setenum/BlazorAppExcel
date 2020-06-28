d947ec23c4335bb2d31a7fea906876336eb50bca
setenum <setenum@users.noreply.github.com>
2020-06-28 16:23:38 +0000

    deploy: 5ace0f6c7af9ee0b09de3d3b006710f07ccdae5a

---
+++ saveAsFile.js
@@ -0,0 +1,8 @@
+﻿function saveAsFile(fileName, base64Str) {
+    var a = document.createElement('a');
+    a.download = fileName;
+    a.href = "data:application/octet-stream;base64," + base64Str;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
